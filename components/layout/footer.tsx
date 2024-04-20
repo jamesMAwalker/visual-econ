bcd2fe085ec4aff525615129aeb103c3b1594a3e
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-04-20 22:22:43 +0700

    feat(layout): :lipstick: Add footer.

---
+++ components/layout/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className='border-neutral-800 gap-md flex items-start justify-start w-full p-8 border-t'>
+      <a href='github.com'>View Repo</a>
+      <span>|</span>
+      <a href='codesandbox.io'>Open in Sandbox</a>
+    </footer>
+  )
+}
