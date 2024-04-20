5867c26d2072f9b6bbfcf3852ccd3bf20c00aa78
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-04-20 14:49:23 +0700

    fix(theme): :pencil2: Fix missing slash in import.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-import "@/styleglobal.scss";
+import "@/style/global.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
