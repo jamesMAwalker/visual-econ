f8b9ff2b98cfba3ecab6c3bc0f65d421fb8a0a02
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-04-24 01:51:37 +0700

    feat(home): :sparkles: Add dropdown and select button.
    Add a dropdown element and button that navigates users to the dashboard.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,45 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+import { GEOFIPS } from '@/data'
+import { STAT_TABLES } from '@/data'
+
+import { Button } from '@/shadcn/ui/button'
+import { Dropdown } from '@/shadcn/ui/dropdown'
+import { useChartInputState } from '@/context/chart-params.context'
+
+// TODO: Add dropdowns for table selection.
+
 export default function Home() {
+  const { push } = useRouter()
+
+  const { stateParams } = useChartInputState()
+
+  function handleSetUrlParams() {
+    const urlTableParams = STAT_TABLES.map(table => `${table.TableName}_${table.LineCode}`).join('&tables=')
+    const urlStateParams =  stateParams.map(item => `${item.key}`).join('&states=')
+    const url = `/dashboard?states=${urlStateParams}&tables=${urlTableParams}`
+
+    push(url)
+  }
+
   return (
     <main className='START_PAGE full flex-col-c gap-md'>
       <div className='BLURB_CONTAINER flex-col-c gap-md w-4/5 lg:w-2/3'>
-        <h1 className='text-2xl lg:text-4xl font-bold text-center'>Visual Economy</h1>
+        <h1 className='text-2xl lg:text-4xl font-bold text-center'>
+          Visual Economy
+        </h1>
         <p className='BLURB font-bold flex-col-c gap-md text-lg lg:text-xl text-center'>
           <span>
             Compare the economic performance and viability of up to 3 different
             states at a time and see how they stack up against each other.
           </span>
         </p>
+      </div>
+      <div className='DROPDOWN_CONTAINER flex-c gap-md'>
+        <Dropdown list={GEOFIPS} />
+        <Button onClick={handleSetUrlParams}>Compare →</Button>
       </div>
     </main>
   )
