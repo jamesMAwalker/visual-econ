34a4e8c4a607c5f3759266c868c9448940a3cc2f
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-04-24 04:22:36 +0700

    test(testing): :white_check_mark: Add global mocks.
    Add ResizeObserver and scrollIntoView to global mocks.

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -2,3 +2,11 @@ import '@testing-library/jest-dom/vitest';
 import React from 'react';
 
 global.React = React;
+
+global.ResizeObserver = vi.fn().mockImplementation(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn(),
+}))
+
+window.HTMLElement.prototype.scrollIntoView = vi.fn();
