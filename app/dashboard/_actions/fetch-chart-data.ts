2e39fc0e3e16c04f4247974b2be0415d5d6da723
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-04-24 22:50:06 +0700

    refactor(api): :recycle: Refactor for clarity; add types.
    Move fetch to external function, localize error handling to fetch. Add explicit types for instances of ": any".

--- app/dashboard/_actions/fetch-chart-data.ts
+++ app/dashboard/_actions/fetch-chart-data.ts
@@ -1,43 +1,51 @@
-export async function fetchChartData(params: any) {
+import { IBeaRequestParams } from "@/lib/convert-params-to-request"
+
+interface ITableAndStateParams {
+  tableName: string;
+  lineCode: string;
+  geoFips: string;
+}
+
+export async function fetchChartData(params: IBeaRequestParams): Promise<(IBeaApiResponse | undefined)[][]> {
   const { stateRequestParams, tableRequestParams } = params
 
+  const tableAndStateParams: ITableAndStateParams[][] = tableRequestParams.map((table: any) => {
+    return stateRequestParams.map((state: any) => {
+      return { ...table, ...state }
+    })
+  })
+
+  const results = await Promise.all(tableAndStateParams.map(async (table) => {
+    return await Promise.all(table.map(async (state) => {
+      return fetchData(state)
+    }))
+  }))
+
+  return results;
+}
+
+async function fetchData(requestParams: ITableAndStateParams): Promise<IBeaApiResponse | undefined> {
   try {
-    const tableAndStateParams = tableRequestParams.map((table: any) => {
-      return stateRequestParams.map((state: any) => {
-        return { ...table, ...state }
-      })
+    const res = await fetch('/api/chart-data', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(requestParams)
     })
 
-    const results = await Promise.all(tableAndStateParams.map(async (table: any) => {
-      return await Promise.all(table.map(async (state: any) => {
-        const { tableName, lineCode, geoFips }  = state;
+    const data = await res.json()
 
-        const res = await fetch('/api/chart-data', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            geoFips,
-            tableName,
-            lineCode
-          })
-        })
+    if (data.status !== 200) {
+      throw new Error(data.message)
+    }
 
-        const data = await res.json()
-
-        if (data.status !== 200) {
-          throw new Error(data.message)
-        }
-
-        return data;
-      }))
-    }))
-
-    console.log("🚀 ~ fetchChartData ~ results:", results)
-    return results;
-  } catch (error) {
-    console.log("🚀 ~ fetchChartData ~ error:", error)
-    throw error;
+    return {
+      ...data.BEAAPI.Results,
+      StateName: data.BEAAPI.Results.Data[0].GeoName,
+      TableName: requestParams.tableName
+    }
+  } catch (error: Error | any) {
+    throw new Error(error.message)
   }
-}
+} 
