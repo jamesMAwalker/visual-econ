5b2e6ef0376635f4b1f5f9a5b10a55d1c6ecf5e8
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-04-24 22:53:56 +0700

    refactor(api): :label: Add new types and apply.
    Also fix missing brackets in tableRequestParams return. Add error case.

--- lib/convert-params-to-request.ts
+++ lib/convert-params-to-request.ts
@@ -1,7 +1,17 @@
+export interface IBeaSearchParams { [key: string]: string | string[] | undefined }
+
+export interface IBeaRequestParams {
+  stateRequestParams: { geoFips: string }[];
+  tableRequestParams: { tableName: string; lineCode: string }[]
+}
+
+
 export function convertParamsToRequest(
-  searchParams: Record<string, string | string[]>
-) {
+  searchParams: IBeaSearchParams
+): IBeaRequestParams {
   const { states, tables } = searchParams
+
+  if (!states || !tables) throw new Error('States and tables params are required')
 
   const stateRequestParams = Array.isArray(states)
     ? states.map((state: any) => ({
@@ -18,7 +28,7 @@ export function convertParamsToRequest(
     : (() => {
       const [tableName, lineCode] = tables.split('_')
 
-      return { tableName, lineCode }
+      return [{ tableName, lineCode }]
     })()
 
   return { stateRequestParams, tableRequestParams }
