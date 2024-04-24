815e9fe63675e7e8dde85e45e4ea165ed767752e
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-04-24 23:19:15 +0700

    feat(dashboard): :sparkles: Add line chart component.

---
+++ components/charts/line.tsx
@@ -0,0 +1,51 @@
+import ChartJS from 'chart.js/auto'
+import { CategoryScale } from 'chart.js'
+import { Line } from 'react-chartjs-2'
+import type { ChartData, ChartOptions } from 'chart.js'
+
+import { CHART_COLORS } from '@/data/colors'
+
+ChartJS.register(CategoryScale)
+
+interface ILineChartProps {
+  dataSets?: any
+  scale?: number
+}
+
+export const LineChart = ({ dataSets, scale }: ILineChartProps) => {
+  const options: ChartOptions<'line'> = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        labels: {
+          boxWidth: 5,
+          boxHeight: 5,
+          color: 'white'
+        }
+      },
+      title: {
+        display: true,
+        text: dataSets[0]?.Statistic
+      }
+    }
+  }
+
+  const data: ChartData<'line'> = {
+    labels: dataSets[0]?.Data?.map((d: any) => d.TimePeriod),
+    datasets: dataSets.map((set: any, idx: number) => {
+      const lineColor = CHART_COLORS[idx].hex
+
+      return {
+        label: set?.StateName,
+        data: set?.Data?.sort((a: IBeaDataItem, b: IBeaDataItem) => {
+          return a.DataValue > b.DataValue
+        })?.map((d: any) => Number(d.DataValue) / 1_000_000_000),
+        borderColor: lineColor,
+        backgroundColor: 'white'
+      }
+    })
+  }
+
+  return <Line options={options} data={data} />
+}
