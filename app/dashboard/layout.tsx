e73c353bc4d1eda7396643828bd3282ce545591b
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-04-24 23:17:10 +0700

    refactor(dashboard): :lipstick: Update layout styling.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,7 +3,11 @@ import { ReactNode } from 'react'
 // TODO: Add layout
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
-  return <div className='full flex-c'>{children}</div>
+  return (
+    <div className='DASHBOARD full grid grid-cols-[.6fr_.4fr] grid-rows-1 divide-x-[.5px] divide-neutral-800 divide-dashed'>
+      {children}
+    </div>
+  )
 }
 
 export default DashboardLayout
