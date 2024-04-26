5950aa605ea9acbd1187c7f284fca233d3f4ea03
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-04-26 21:24:39 +0700

    refactor(dashboard): :sparkles: Add charts list and summary; update useSWR; error handling.
    Include list and summary components. Revalidate whenever urlSearchParams changes (useSWR). Handle errors and loading state.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,37 +1,46 @@
 'use client'
 
-import React from 'react'
 import useSWR from 'swr'
+import { PageProps } from '@/.next/types/app/dashboard/page'
 
 import { useChartInputState } from '@/context/chart-params.context'
+
 import { fetchChartData } from './_actions/fetch-chart-data'
-import { convertParamsToRequest } from '@/lib/convert-params-to-request'
 
-// TODO: Get data using params.
-// TODO: Render data in layout.
+import { ChartsList } from './_components/chart-list'
+import { ChartsSummary } from './_components/chart-summary'
 
-const Dashboard = ({ searchParams }: any) => {
+
+const Dashboard = ({ searchParams }: PageProps) => {
   const { stateParams } = useChartInputState()
 
   const {
     error,
     data,
     isLoading,
-    mutate: refetch
   } = useSWR(
-    stateParams ? '/chart-data' : null,
-    () => fetchChartData({ ...convertParamsToRequest(searchParams) }),
-    {
-      shouldRetryOnError: false
-    }
+    stateParams ? searchParams : null,
+    () => fetchChartData(searchParams)
   )
 
+  if (error) throw new Error('No chart data loaded!')
+
+  if (isLoading || !data || data?.length === 0) {
+    return (
+      <>
+        <ChartsList.Skeleton />
+        <ChartsSummary.Skeleton />
+      </>
+    )
+  }
+
+  const assertedData = data as IBeaApiResponse[][];
+
   return (
-    <main className='full flex-col-c gap-md'>
-      {/* {searchParams.states.map((state: any) => {
-        return <span key={state}>{state}</span>
-      })} */}
-    </main>
+    <>
+      <ChartsList data={assertedData} />
+      <ChartsSummary data={assertedData} />
+    </>
   )
 }
 
