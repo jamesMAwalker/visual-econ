4845008dfa09ba22ae604f7d89b884efab6e9d8c
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-04-26 21:02:59 +0700

    refactor(api): :recycle: Refactor to scale based on data value size.

--- lib/get-mean-from-bea.ts
+++ lib/get-mean-from-bea.ts
@@ -1,57 +1,32 @@
 /**
- * * getMeanFromDataSet
- * Calculates the mean value from a given data set, with optional scaling and number of years.
+ * * getMeanFromBeaData *
+ * Calculates a mean value from an array of BEA data items.
  *
- * @param dataSet - The data set to calculate the mean from.
- * @param scalingKey - The scaling factor to apply to the data values, can be 'BN', 'M', 'K', or 'NONE'.
- * @returns The mean value of the provided number of data years, formatted with the appropriate scaling abbreviation.
+ * @param data - An array of `IBeaDataItem` objects containing the data to calculate the mean from.
+ * @returns A string representing the mean value, with an appropriate scaling factor (e.g. "1.2k", "4.5m", "2.84bn").
  */
 
-interface IAverageArgs {
-  dataSet: IBeaApiResponse
-  scalingKey?: 'BN' | 'M' | 'K' | 'NONE'
+export function getMeanFromBeaData({ data }: { data: IBeaDataItem[] }) {
+  if (data.length <= 0) return
+
+  const sum = data.reduce(
+    (acc: number, curr: IBeaDataItem) => acc + Number(curr.DataValue),
+    0
+  )
+  const mean = sum / data.length
+
+  const scalingFactors = [
+    { factor: 1_000_000_000, abbr: 'bn', digits: 2 },
+    { factor: 1_000_000, abbr: 'm', digits: 1 },
+    { factor: 1_000, abbr: 'k', digits: 0 },
+    { factor: 1, abbr: '', digits: 0 }
+  ]
+
+  const { factor, abbr, digits } =
+    scalingFactors.find(({ factor }) => mean >= factor) ||
+    scalingFactors[scalingFactors.length - 1]
+
+  const refinedMean = Number((mean / factor).toFixed(digits))
+
+  return `${refinedMean}${abbr}`
 }
-
-export function getMeanFromBeaData({
-  dataSet,
-  scalingKey = 'M',
-}: IAverageArgs) {
-  const scaling: { [key: string]: { factor: number; abbr: string, digits: number } } = {
-    BN: {
-      factor: 1_000_000_000,
-      abbr: 'bn',
-      digits: 1
-    },
-    M: {
-      factor: 1_000_000,
-      abbr: 'm',
-      digits: 1
-    },
-    K: {
-      factor: 1_000,
-      abbr: 'k',
-      digits: 0
-    },
-    NONE: {
-      factor: 1,
-      abbr: '',
-      digits: 0
-
-    }
-  }
-
-  if (dataSet.Data.length <= 0) return;
-
-  const sum = dataSet?.Data.reduce((acc: number, curr: IBeaDataItem) => {
-    return acc + Number(curr.DataValue)
-  }, 0)
-
-  const mean = sum / dataSet.Data.length
-
-  const refinedMean = Number((mean / scaling[scalingKey].factor).toFixed(scaling[scalingKey].digits))
-
-  return `${refinedMean}${scaling[scalingKey].abbr}`
-}
-
-
-
