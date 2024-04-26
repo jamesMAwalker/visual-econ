ca4064f8fd315395d6bef3240649eedfee7c0c62
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-04-26 21:21:27 +0700

    refactor(api): :recycle: Refactor to include dates.
    Use new helper function to get dates in proper format. Add documentation.

--- lib/convert-params-to-request.ts
+++ lib/convert-params-to-request.ts
@@ -1,35 +1,51 @@
-export interface IBeaSearchParams { [key: string]: string | string[] | undefined }
+/**
+ * * convertParamsToRequest *
+ * Converts search parameters to request parameters for the BEA API.
+ *
+ * @param searchParams - An object containing the search parameters, including states, tables, and dates.
+ * @returns An object containing the request parameters, including state request params, table request params, and date request params.
+ * @throws Error if the state, table, or date parameters are missing.
+ */
 
-export interface IBeaRequestParams {
-  stateRequestParams: { geoFips: string }[];
-  tableRequestParams: { tableName: string; lineCode: string }[]
+import { convertDateRangeToStringList } from "./convert-date-range-to-string-list";
+
+export interface IBeaSearchParams {
+  [key: string]: string | string[] | undefined
 }
 
+export interface IBeaRequestParams {
+  stateRequestParams: { geoFips: string }[]
+  tableRequestParams: { tableName: string; lineCode: string }[]
+  dateRequestParams: { years: string }
+}
 
 export function convertParamsToRequest(
   searchParams: IBeaSearchParams
 ): IBeaRequestParams {
-  const { states, tables } = searchParams
+  const { states, tables, dates } = searchParams
 
-  if (!states || !tables) throw new Error('States and tables params are required')
+  if (!states || !tables || !dates)
+    throw new Error('State, table, and date params are required.')
 
+  const dateRequestParams = Array.isArray(dates)
+    ? convertDateRangeToStringList(dates)
+    : { years: dates }
   const stateRequestParams = Array.isArray(states)
     ? states.map((state: any) => ({
-      geoFips: state
-    }))
+        geoFips: state
+      }))
     : [{ geoFips: states }]
-
   const tableRequestParams = Array.isArray(tables)
     ? tables.map((table: any) => {
-      const [tableName, lineCode] = table.split('_')
+        const [tableName, lineCode] = table.split('_')
 
-      return { tableName, lineCode }
-    })
+        return { tableName, lineCode }
+      })
     : (() => {
-      const [tableName, lineCode] = tables.split('_')
+        const [tableName, lineCode] = tables.split('_')
 
-      return [{ tableName, lineCode }]
-    })()
+        return [{ tableName, lineCode }]
+      })()
 
-  return { stateRequestParams, tableRequestParams }
+  return { stateRequestParams, tableRequestParams, dateRequestParams }
 }
