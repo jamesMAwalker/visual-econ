18a0051223d3800edf693f2c859d057f5cd82157
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-04-27 01:43:25 +0700

    refactor(theme): :lipstick: Update layout components for responsiveness.
    Layout components now responsive; add shadcn sheet for mobile input menu.

---
+++ components/layout/mobile-toolbar.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from 'react'
+import { Settings2 } from 'lucide-react'
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger
+} from '@/shadcn/ui/sheet'
+import { Button } from '@/shadcn/ui/button'
+
+
+export function MobileToolbar({ children }: { children: ReactNode }) {
+  return (
+    <Sheet>
+      <SheetTrigger asChild className='MOBILE_TOOLS_TRIGGER block lg:hidden'>
+        <Button variant='outline'>
+          <Settings2 />
+        </Button>
+      </SheetTrigger>
+      <SheetContent
+        side='top'
+        className='MOBILE_TOOLS flex-col-tl p-lg gap-md'
+      >
+        <SheetTitle>Chart Parameters</SheetTitle>
+        <SheetDescription>
+          Change the chart parameters using the dropdowns below.
+        </SheetDescription>
+        {children}
+      </SheetContent>
+    </Sheet>
+  )
+}
--- components/layout/toolbar.tsx
+++ components/layout/toolbar.tsx
@@ -1,16 +1,26 @@
 'use client'
 
-import Link from 'next/link'
+import { MouseEventHandler, ReactNode } from 'react'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import Link from 'next/link'
 
 import { GEOFIPS, STAT_TABLES } from '@/data'
 
 import { useChartInputState } from '@/context/chart-params.context'
 
 import { Button } from '@/shadcn/ui/button'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger
+} from '@/shadcn/ui/sheet'
+
 import { StateDropdown } from '@/components/inputs/state-dropdown'
 import { DateDropdown } from '@/components/inputs/date-dropdown'
 import { useGetInitialStateFromParams } from '@/app/dashboard/_hooks/useGetInitialStateFromParams'
+import { Settings2 } from 'lucide-react'
 
 export const Toolbar = () => {
   const path = usePathname()
@@ -21,7 +31,6 @@ export const Toolbar = () => {
   useGetInitialStateFromParams()
 
   function handleSetUrlParams() {
-      
     const urlDateParams = dateParams
       .map((date) => `${date.value}`)
       .join('&dates=')
@@ -38,19 +47,64 @@ export const Toolbar = () => {
   }
 
   return (
-    <div className='TOOLBAR md:justify-between md:flex-row border-neutral-800 z-50 flex flex-col items-start justify-start w-full gap-8 p-8 border-b h-min'>
+    <div className='TOOLBAR justify-between border-neutral-800 z-50 flex items-start w-full gap-lg p-layout border-b h-min'>
       <Link href='/'>
         <h1 className='LOGO text-2xl font-bold'>Visual Economy</h1>
       </Link>
       {path === '/dashboard' && (
-        <div className='TOOLS sm:flex-row md:w-auto flex flex-col items-center justify-center w-full gap-4'>
-          <DateDropdown />
-          <StateDropdown list={GEOFIPS} />
-          <Button onClick={handleSetUrlParams} variant='secondary'>
-            Submit
-          </Button>
-        </div>
+        <>
+          <div className='DESKTOP_TOOLS hidden lg:flex lg:items-center lg:justify-end w-min gap-4'>
+            <Toolbar.Tools handler={handleSetUrlParams}/>
+          </div>
+          <Toolbar.Mobile>
+            <Toolbar.Tools handler={handleSetUrlParams}/>
+          </Toolbar.Mobile>
+        </>
       )}
     </div>
   )
 }
+
+Toolbar.Desk = function DesktopToolbar({ children }: { children: ReactNode }) {
+  return (
+    <div className='DESKTOP_TOOLS hidden lg:flex lg:items-center lg:justify-end w-min gap-4'>
+      {children}
+    </div>
+  )
+}
+
+Toolbar.Mobile = function MobileToolbar({ children }: { children: ReactNode }) {
+  return (
+    <Sheet>
+      <SheetTrigger asChild className='MOBILE_TOOLS_TRIGGER block lg:hidden'>
+        <Button variant='outline'>
+          <Settings2 />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side='top' className='MOBILE_TOOLS flex-col-tl p-lg gap-md'>
+        <SheetTitle>Chart Parameters</SheetTitle>
+        <SheetDescription>
+          Change the chart parameters using the dropdowns below.
+        </SheetDescription>
+        {children}
+      </SheetContent>
+    </Sheet>
+  )
+}
+
+Toolbar.Tools = function Tools({
+  handler
+}: {
+  handler: MouseEventHandler<HTMLButtonElement>
+}) {
+  return (
+    <>
+      <DateDropdown />
+      <StateDropdown list={GEOFIPS} />
+      <Button onClick={handler} variant='secondary'>
+        Submit
+      </Button>
+    </>
+  )
+}
+
