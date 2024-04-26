df57c8d24157b381b825b8d63a008cc42d21fcc6
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-04-26 21:19:47 +0700

    feat(dashboard): :sparkles: Add click handler for getting state from URL.

--- components/layout/toolbar.tsx
+++ components/layout/toolbar.tsx
@@ -1,12 +1,42 @@
 'use client'
 
-import { Button } from '@/shadcn/ui/button'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+
+import { GEOFIPS, STAT_TABLES } from '@/data'
+
+import { useChartInputState } from '@/context/chart-params.context'
+
+import { Button } from '@/shadcn/ui/button'
+import { StateDropdown } from '@/components/inputs/state-dropdown'
+import { DateDropdown } from '@/components/inputs/date-dropdown'
+import { useGetInitialStateFromParams } from '@/app/dashboard/_hooks/useGetInitialStateFromParams'
 
 export const Toolbar = () => {
   const path = usePathname()
-  
+  const router = useRouter()
+
+  const { stateParams, dateParams } = useChartInputState()
+
+  useGetInitialStateFromParams()
+
+  function handleSetUrlParams() {
+      
+    const urlDateParams = dateParams
+      .map((date) => `${date.value}`)
+      .join('&dates=')
+    const urlTableParams = STAT_TABLES.map(
+      (table) => `${table.TableName}_${table.LineCode}`
+    ).join('&tables=')
+    const urlStateParams = stateParams
+      .map((item) => `${item.key}`)
+      .join('&states=')
+    const url = `/dashboard?states=${urlStateParams}&tables=${urlTableParams}&dates=${urlDateParams}`
+
+    router.push(url)
+    router.refresh()
+  }
+
   return (
     <div className='TOOLBAR md:justify-between md:flex-row border-neutral-800 z-50 flex flex-col items-start justify-start w-full gap-8 p-8 border-b h-min'>
       <Link href='/'>
@@ -14,7 +44,11 @@ export const Toolbar = () => {
       </Link>
       {path === '/dashboard' && (
         <div className='TOOLS sm:flex-row md:w-auto flex flex-col items-center justify-center w-full gap-4'>
-          <Button variant='outline'>Submit</Button>
+          <DateDropdown />
+          <StateDropdown list={GEOFIPS} />
+          <Button onClick={handleSetUrlParams} variant='secondary'>
+            Submit
+          </Button>
         </div>
       )}
     </div>
