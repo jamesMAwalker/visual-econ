df35f1d7250a82e2bd553cd0adbebd58dd30a82d
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-04-28 20:21:22 +0700

    refactor(dashboard): :lipstick: Fix responsive issue.
    Grid layout styling simplified.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,9 @@
 import { ReactNode } from 'react'
 
-// TODO: Add layout
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className='DASHBOARD full grid grid-cols-[.6fr_.4fr] grid-rows-1 divide-x-[.5px] divide-neutral-800 divide-dashed'>
+    <div className='DASHBOARD full grid grid-cols-1 grid-rows-[auto_1fr] lg:grid-cols-[.6fr_.4fr] lg:grid-rows-1 divide-x-[.5px] divide-neutral-800 divide-dashed'>
       {children}
     </div>
   )
