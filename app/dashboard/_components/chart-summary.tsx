ca3f2a72ef1aaf485c95bc924640a23dcaceb0ac
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-04-28 20:07:02 +0700

    refactor(dashboard): :lipstick: Add responsiveness.
    Replace component when below desktop vp. Uses sheet component to stow extra ui.

--- app/dashboard/_components/chart-summary.tsx
+++ app/dashboard/_components/chart-summary.tsx
@@ -1,85 +1,96 @@
 'use client'
 
+import { ChevronDown } from 'lucide-react'
 
 import { useChartInputState } from '@/context/chart-params.context'
-import { useGetCurrentDataSummary } from '../_hooks/useGetCurrentDataSummary'
+import {
+  IChartSummaryInfo,
+  useGetCurrentDataSummary
+} from '../_hooks/useGetCurrentDataSummary'
 
-import { cn } from '@/shadcn/utils'
-import { getMeanFromBeaData } from '@/lib/get-mean-from-bea'
-
-import { CHART_COLORS } from '@/data/colors'
-import { IDataDescription } from '@/data/descriptions'
-
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger
+} from '@/shadcn/ui/sheet'
 import { Skeleton } from '@/shadcn/ui/skeleton'
+import { Button } from '@/shadcn/ui/button'
+
+import { ChartTextSummary } from './chart-text-summary'
+import { ChartDataSummary } from './chart-data-summary'
 
 
 export function ChartsSummary({ data }: { data: IBeaApiResponse[][] }) {
   const { currentChartId } = useChartInputState()
 
-  const summary = useGetCurrentDataSummary(data, currentChartId)
-
-  return (
-    <div className='SUMMARY flex-col-tl bg-zinc-500/10 full divide-y-[.5px] divide-neutral-800 divide-dashed'>
-      {summary?.currentDescription && (
-        <TextSummary description={summary.currentDescription} />
-      )}
-      {summary?.currentData && <DataSummary data={summary.currentData} />}
-    </div>
+  const summary: IChartSummaryInfo | null = useGetCurrentDataSummary(
+    data,
+    currentChartId
   )
-}
 
-function TextSummary({ description }: { description: IDataDescription }) {
+  if (!summary) return null
+
   return (
-    <div className='CURRENT_DATA flex-col-tl gap-md p-lg h-2/3 w-full'>
-      <div className='HEADING text-2xl font-bold'>{description?.title}</div>
-      <p className='DESCRIPTION'>{description?.description}</p>
-      <ul className='LIST list-style-disc'>
-        <li className='text-lg italic font-bold'>Key Features</li>
-        {description?.key_features.map((feature: string) => {
-          return <li key={feature}>{feature}</li>
-        })}
-      </ul>
-    </div>
-  )
-}
-
-function DataSummary({ data }: { data: IBeaApiResponse[] }) {
-  return (
-    <div className='OVERALL_DATA flex-col-c gap-md p-lg h-1/3 w-full'>
-      <p className='flex-col-c uppercase text-[12px] text-center'>
-        <span>{data[0].Statistic}</span>
-        <span>(Time Period Average)</span>
-      </p>
-      <div className='NUMBERS_CONTAINER flex-c gap-md'>
-        {data.map((data: IBeaApiResponse, idx: number) => {
-          const mean = getMeanFromBeaData({ data: data.Data })
-
-          return (
-            <div
-              key={data.StateName}
-              style={{ color: `${CHART_COLORS[idx].hex}` }}
-              className='flex-col-c'
-            >
-              <span className='text-[14px] uppercase'>{data.StateName}</span>
-              <span className={cn('text-2xl font-bold uppercase')}>
-                {mean}
-              </span>
-            </div>
-          )
-        })}
+    <>
+      <div className='DESKTOP_SUMMARY hidden lg:flex order-first lg:order-last h-full flex-row lg:flex-col items-start justify-start bg-zinc-500/10 full divide-y-[.5px] divide-neutral-800 divide-dashed'>
+        {summary?.currentDescription && (
+          <ChartTextSummary description={summary.currentDescription} />
+        )}
+        <ChartDataSummary data={summary.currentData} />
       </div>
+      <ChartsSummary.Mobile summary={summary} />
+    </>
+  )
+}
+
+ChartsSummary.Mobile = function MobileSummary({
+  summary
+}: {
+  summary: IChartSummaryInfo
+}) {
+  const { currentDescription: description, currentData: data } = summary
+
+  return (
+    <div className='MOBILE_SUMMARY order-first lg:order-last flex lg:hidden items-center justify-between p-layout'>
+      <Sheet>
+        <SheetTrigger asChild className='MOBILE_SUMMARY_TRIGGER lg:hidden'>
+          <SheetHeader className='full flex flex-row items-center justify-between'>
+            <span>{description.title}</span>
+            <Button variant='outline'>
+              <ChevronDown />
+            </Button>
+          </SheetHeader>
+        </SheetTrigger>
+        <SheetContent
+          side='bottom'
+          className='MOBILE_TOOLS flex-col-tl p-lg gap-md'
+        >
+          <SheetTitle>{description.title}</SheetTitle>
+          <SheetDescription>{description.description}</SheetDescription>
+          <ul className='LIST list-style-disc hidden lg:block'>
+            <li className='text-lg italic font-bold'>Key Features</li>
+            {description?.key_features.map((feature: string) => {
+              return <li key={feature}>{feature}</li>
+            })}
+          </ul>
+          <ChartDataSummary data={data} />
+        </SheetContent>
+      </Sheet>
     </div>
   )
 }
 
 ChartsSummary.Skeleton = function SkeletonSummary() {
   return (
-    <div className='SKELETON_SUMMARY flex-col-tl full divide-y-[.5px] divide-neutral-800 divide-dashed'>
-      <div className='SKELETON_TEXT flex-col-tl p-lg gap-md h-2/3 w-full'>
+    <div className='SKELETON_SUMMARY lg:hidden flex-col-tl full divide-y-[.5px] divide-neutral-800 divide-dashed'>
+      <div className='SKELETON_TEXT flex-col-tl p-lg gap-md full'>
         <Skeleton className='h-[5ch] w-full' />
         <Skeleton className='w-full h-full' />
       </div>
-      <div className='SUMMARY_SKELETON flex-c p-lg gap-md h-1/3 w-full'>
+      <div className='SUMMARY_SKELETON flex-c p-lg gap-md h-1/4 full'>
         <Skeleton className='aspect-square w-full' />
         <Skeleton className='aspect-square w-full' />
         <Skeleton className='aspect-square w-full' />
