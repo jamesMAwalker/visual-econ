4bec52c0758503c520f4ddba1fb769f12af63655
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-04-29 21:44:25 +0700

    feat(layout): :sparkles: Add loading screens.

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import { BarChart2 } from "lucide-react"
+
+const Loading = () => {
+  return (
+    <div className='full flex-c animate-pulse'>
+      <BarChart2 />
+    </div>
+  )
+}
+
+export default Loading;
