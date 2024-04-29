1548332503237f20a4c49d4b5978f781fb62e21e
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-04-29 22:36:55 +0700

    docs(dashboard): :memo: Add documentation for custom hook.

--- app/dashboard/_hooks/useSetUrlParams.tsx
+++ app/dashboard/_hooks/useSetUrlParams.tsx
@@ -1,13 +1,24 @@
-import { useRouter } from "next/navigation"
+/**
+ * * useSetUrlParams *
+ * A custom hook that generates URL search params from user inputs.
+ *
+ * The hook gets the user's desired search params from context, and builds a URL string. It then uses the push method to take the user to the dashboard page with 
+ * their search paramters carried into the next page via the URL.
+ * 
+ * We also call the refresh method, as push does not reset the page state by default. Calling refresh allows us to display new chart data whenever users update the 
+ * toolbar inputs.
+ */
 
-import { useChartInputState } from "@/context/chart-params.context"
-import { STAT_TABLES } from "@/data"
+import { useRouter } from 'next/navigation'
 
+import { useChartInputState } from '@/context/chart-params.context'
+import { STAT_TABLES } from '@/data'
 
 export function useSetUrlParams() {
   const router = useRouter()
 
   const { stateParams, dateParams } = useChartInputState()
+  
   function handleSetUrlParams() {
     const urlDateParams = dateParams
       .map((date) => `${date.value}`)
@@ -19,13 +30,10 @@ export function useSetUrlParams() {
       .map((item) => `${item.key}`)
       .join('&states=')
     const url = `/dashboard?states=${urlStateParams}&tables=${urlTableParams}&dates=${urlDateParams}`
-  
+
     router.push(url)
     router.refresh()
   }
 
-  return { handleSetUrlParams };
+  return { handleSetUrlParams }
 }
-
-
-
